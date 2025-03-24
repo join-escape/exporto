187b20a6eeb8c01685171bc9961f5f40531efe75
Souvik Kar Mahapatra <souvikat001@gmail.com>
2025-03-25 04:55:54 +0530

    chore

--- src/env.mjs
+++ src/env.mjs
@@ -10,15 +10,19 @@ export const env = createEnv({
     AUTH_URL: z.string(),
     AUTH_SECRET: z.string(),
     AUTH_GOOGLE_ID: z.string(),
-    AUTH_GOOGLE_SECRET: z.string()
+    AUTH_GOOGLE_SECRET: z.string(),
+    AUTH_GITHUB_ID: z.string(),
+    AUTH_GITHUB_SECRET: z.string(),
+    AUTH_NOTION_ID: z.string(),
+    AUTH_NOTION_SECRET: z.string(),
+    AUTH_NOTION_REDIRECT_URI: z.string(),
   },
   /*
    * Environment variables available on the client (and server).
    *
    * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
    */
-  client: {
-  },
+  client: {},
   /*
    * Due to how Next.js bundles environment variables on Edge and Client,
    * we need to manually destructure them to make sure all are included in bundle.
@@ -28,6 +32,13 @@ export const env = createEnv({
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    AUTH_NOTION_ID: process.env.AUTH_NOTION_ID,
+    AUTH_NOTION_SECRET: process.env.AUTH_NOTION_SECRET,
+    AUTH_NOTION_REDIRECT_URI: process.env.AUTH_NOTION_REDIRECT_URI,
   },
 });
+
+console.log({ env });
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,17 +1,22 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./db";
 
 export const {
-	handlers: { GET, POST },
-	signIn,
-	signOut,
-	auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
 } = NextAuth({
-	trustHost: true,
-	adapter: DrizzleAdapter(db),
-	providers: [
-		Google
-	],
+  trustHost: true,
+  adapter: DrizzleAdapter(db),
+  providers: [
+    Google,
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
 });
